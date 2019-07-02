0a6abb492bbf47e09af2af58801f380d1b6d13ac
evgenij <zhenyasmagin@yandex.ru>
2019-07-02 14:58:28 +0300

    comment hashcode validator

--- valuetypeanalyzer/src/main/java/com/smagin/valuetypeanalyzer/valuetypeanalyzer/validators/hashcodevalidators/HashCodeValidator.java
+++ valuetypeanalyzer/src/main/java/com/smagin/valuetypeanalyzer/valuetypeanalyzer/validators/hashcodevalidators/HashCodeValidator.java
@@ -1,53 +1,53 @@
-package com.smagin.valuetypeanalyzer.valuetypeanalyzer.validators.hashcodevalidators;
-
-import newapi.api.Validator;
-import newapi.model.Report;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-
-@Component
-public class HashCodeValidator implements Validator {
-
-    @Override
-    public Report validate(ClassNode classNode) {
-        boolean isHashCodeWithoutIdentity = Optional.ofNullable(classNode).map(s -> getMethodNodeHashCode(s.methods))
-                .map(this::validateMethodOnHashCodeCondition).orElse(Boolean.FALSE);
-
-        List<MethodNode> methodNodes = Optional.ofNullable(classNode).map(s -> s.methods).orElse(new ArrayList<>());
-
-        Report report = new Report();
-        report.setResult(isHashCodeWithoutIdentity);
-
-        return report;
-    }
-
-    public MethodNode getMethodNodeHashCode(List<MethodNode> methodNodes) {
-        return methodNodes.stream()
-                .filter(s -> s.name.equals("hashCode"))
-                .findFirst()
-                .orElse(null);
-    }
-
-    public boolean validateMethodOnHashCodeCondition(MethodNode methodNode) {
-        if (methodNode != null) {
-            AbstractInsnNode[] abstractInsnNodes = methodNode.instructions.toArray();
-            for (int i = 0; i < abstractInsnNodes.length; i++) {
-                if (abstractInsnNodes[i].getOpcode() == Opcodes.INVOKESPECIAL) {
-                    MethodInsnNode min = (MethodInsnNode) abstractInsnNodes[i];
-                    return !min.owner.equals("java/lang/Object");
-                }
-            }
-        }
-        return true;
-    }
-
-}
+//package com.smagin.valuetypeanalyzer.valuetypeanalyzer.validators.hashcodevalidators;
+//
+//import newapi.api.Validator;
+//import newapi.model.Report;
+//import org.objectweb.asm.Opcodes;
+//import org.objectweb.asm.tree.AbstractInsnNode;
+//import org.objectweb.asm.tree.ClassNode;
+//import org.objectweb.asm.tree.MethodInsnNode;
+//import org.objectweb.asm.tree.MethodNode;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//
+//@Component
+//public class HashCodeValidator implements Validator {
+//
+//    @Override
+//    public Report validate(ClassNode classNode) {
+//        boolean isHashCodeWithoutIdentity = Optional.ofNullable(classNode).map(s -> getMethodNodeHashCode(s.methods))
+//                .map(this::validateMethodOnHashCodeCondition).orElse(Boolean.FALSE);
+//
+//        List<MethodNode> methodNodes = Optional.ofNullable(classNode).map(s -> s.methods).orElse(new ArrayList<>());
+//
+//        Report report = new Report();
+//        report.setResult(isHashCodeWithoutIdentity);
+//
+//        return report;
+//    }
+//
+//    public MethodNode getMethodNodeHashCode(List<MethodNode> methodNodes) {
+//        return methodNodes.stream()
+//                .filter(s -> s.name.equals("hashCode"))
+//                .findFirst()
+//                .orElse(null);
+//    }
+//
+//    public boolean validateMethodOnHashCodeCondition(MethodNode methodNode) {
+//        if (methodNode != null) {
+//            AbstractInsnNode[] abstractInsnNodes = methodNode.instructions.toArray();
+//            for (int i = 0; i < abstractInsnNodes.length; i++) {
+//                if (abstractInsnNodes[i].getOpcode() == Opcodes.INVOKESPECIAL) {
+//                    MethodInsnNode min = (MethodInsnNode) abstractInsnNodes[i];
+//                    return !min.owner.equals("java/lang/Object");
+//                }
+//            }
+//        }
+//        return true;
+//    }
+//
+//}
