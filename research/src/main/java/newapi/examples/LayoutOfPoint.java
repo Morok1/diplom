2866a8e0d30f5ae0df0f0a81a8686347b850fa43
evgenij <zhenyasmagin@yandex.ru>
2019-07-04 17:09:42 +0300

    add example ClassLayot

--- research/src/main/java/newapi/examples/LayoutOfPoint.java
+++ research/src/main/java/newapi/examples/LayoutOfPoint.java
@@ -1,7 +1,25 @@
 package newapi.examples;
 
+import lombok.AllArgsConstructor;
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LayoutOfPoint {
     public static void main(String[] args) {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(2, 3));
+        points.add(new Point(2, 4));
+        points.add(new Point(2, 5));
 
+        System.out.println(ClassLayout.parseClass(ArrayList.class).toPrintable());
+        System.out.println(ClassLayout.parseClass(ArrayList.class).toPrintable(points));
+    }
+
+    @AllArgsConstructor
+    public static class Point{
+        private int x;
+        private int y;
     }
 }
